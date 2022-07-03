2bd8ea67d1f5bd73cad4380c9a25f8bee3683a2d
jpinto <jfpinto1989@gmail.com>
2022-07-02 20:39:33 -0400

    Put global variables in dedicated object

--- src/script.js
+++ src/script.js
@@ -1,7 +1,23 @@
 import './style.css';
 
-console.log('hello, world!');
-function dummyFunction() {
-  return 1;
-}
-export default dummyFunction;
+const global = {
+  numString: '0',
+  opString: '',
+  numArray: [],
+  opArray: [],
+  lastNum: undefined,
+  lastOp: undefined,
+  error: false,
+  isOpEntry: false,
+  isDecimalEntry: false,
+  isAlreadyDecimal: false,
+  usingPreviousAnswer: false,
+  usedCE: false,
+  alreadyClearedAll: false,
+};
+
+const INTEGER_DIGIT_LIMIT = 9;
+const DECIMAL_DIGIT_LIMIT = 8;
+const RESULT_MIN = 0.00000001;
+const RESULT_MAX = 100000000;
+const OP_NAMES = ['multiply', 'divide', 'add', 'subtract'];
