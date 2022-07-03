a6910e6db7e45eb6cfbcdd75db211563ee6dd064
jpinto <jfpinto1989@gmail.com>
2022-07-02 22:03:02 -0400

    Operate function from old_script w/ relevant var name changes

--- src/script.js
+++ src/script.js
@@ -21,3 +21,29 @@ const DECIMAL_DIGIT_LIMIT = 8;
 const RESULT_MIN = 0.00000001;
 const RESULT_MAX = 100000000;
 const OP_NAMES = ['multiply', 'divide', 'add', 'subtract'];
+
+function operate(num1, num2, operator) {
+  let result;
+  if (operator === 'add') {
+    result = num1 + num2;
+  } else if (operator === 'subtract') {
+    result = num1 - num2;
+  } else if (operator === 'multiply') {
+    result = num1 * num2;
+  } else if (operator === 'divide') {
+    if (num2 === 0) {
+      global.error = true;
+      return undefined;
+    }
+
+    result = num1 / num2;
+  }
+
+  if (result < RESULT_MIN || result > RESULT_MAX) {
+    result = result.toExponential(2);
+  } else {
+    result = Number(result.toFixed(8));
+  }
+
+  return result;
+}
