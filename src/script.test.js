b7186dd270574013c948d99978c6556f47c93c0e
jpinto <jfpinto1989@gmail.com>
2022-07-03 21:40:07 -0400

    Test for fixed point result formatting

--- src/script.test.js
+++ src/script.test.js
@@ -6,3 +6,12 @@ test('verify basic operations are working', () => {
   expect(operate(-5, 3, 'x')).toEqual(-15);
   expect(operate(4.2, 2, '/')).toEqual(2.1);
 });
+
+test('operate function returns undefined when dividing by zero', () => {
+  expect(operate(1, 0, '/')).toEqual(undefined);
+});
+
+test('operate formats results to fixed point 8 decimal places w/ correct rounding', () => {
+  expect(operate(0.00000001, 0.000000015, '+')).toEqual(0.00000002);
+  expect(operate(999999999, 0.000000014, '+')).toEqual(999999999.00000001);
+});
